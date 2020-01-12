c444f16d7eef3c608fd6b76550f28cfcf56f58f0
Joost Diepenmaat <joost@zeekat.nl>
2020-01-12 19:57:00 +0100

    update url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nl.zeekat/ring-openapi-validator "0.1.0"
   :description "Validate ring requests and responses against Swagger/OpenAPI"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/zeekat/ring-openapi-validator"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
